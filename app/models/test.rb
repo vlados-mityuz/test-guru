1b284a6f021b88b54b8c646effccc35f1316f4bf
Vladislav <thenoma1@gmail.com>
2021-09-13 20:30:50 +0300

    Lesson 4 ver. 2

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   has_many: users
+  has_many: categories
 
   def self.tests_by_category(category_title)
     Test.where("title = ?", category_title).order("title DESC")  
