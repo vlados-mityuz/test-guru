ba3be2426ae1250351c9f9b030f1e02f08252d6a
Vladislav <thenoma1@gmail.com>
2021-09-14 19:23:41 +0300

    Missing category in where fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,6 @@ class Test < ApplicationRecord
   has_many: category
 
   def self.tests_by_category(category_title)
-    Test.joins(:category).where("title = ?", category_title).order("title DESC").pluck(:category_title)
+    Test.joins(:category).where(category: {title: category_title}).order("title DESC").pluck(:category_title)
   end
 end
