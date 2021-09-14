b4b333ac3356ccc95b3cfdf36f6bb076ddc0335a
Vladislav <thenoma1@gmail.com>
2021-09-14 19:15:34 +0300

    Lesson 4 ver 3

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   has_many: users
-  has_many: categories
+  has_many: category
 
   def self.tests_by_category(category_title)
-    Test.where("title = ?", category_title).order("title DESC")  
+    Test.joins(:category).where("title = ?", category_title).order("title DESC").pluck(:category_title)
   end
 end
