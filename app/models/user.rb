752333a7f81b9eefd4f39920910594f5b3fc6fbd
Vladislav <thenoma1@gmail.com>
2021-09-19 13:21:22 +0300

    Delete unnecessary association in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :created_tests, class_name: 'Test', foreign_key: :creator_id
   has_and_belongs_to_many :tests, through: :tests_users
-  has_and_belongs_to_many :tests, through: :creator_id
 
   def show_passed_tests(level)
     tests.where(level: level)
