97387f317f28e7c5ef29af4bd415be5b5e511966
Vladislav <thenoma1@gmail.com>
2021-09-21 18:04:09 +0300

    Update questions views

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header(question, test)
+    if question.new_record?
+      "Create new '#{test.title}' question"
+    elsif question.persisted?
+      "Edit '#{test.title}' question"
+    end
+  end
 end
