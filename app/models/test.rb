3e71d8a5a726ab9e14e3738248de3c60aa00829d
Vladislav <thenoma1@gmail.com>
2021-09-23 00:19:41 +0300

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
-  has_many :tests_users
   belongs_to :creator, class_name: 'User', foreign_key: :creator_id
-  has_and_belongs_to_many :users, through: :tests_users
+  has_many :test_passages
+  has_many :users, through: :test_passages
 
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { greater_than: 0 }, uniqueness: { scope: :title }
