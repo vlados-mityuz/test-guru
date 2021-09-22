7fd8431c134abc9876f41af36c1ae846d642d47f
Vladislav <thenoma1@gmail.com>
2021-09-23 00:17:31 +0300

    Update test_passages table

--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,11 @@ ActiveRecord::Schema.define(version: 2021_09_20_072800) do
   create_table "test_passages", force: :cascade do |t|
     t.integer "test_id"
     t.integer "user_id"
+    t.integer "current_question_id"
+    t.integer "correct_questions", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
     t.index ["user_id"], name: "index_test_passages_on_user_id"
   end
@@ -65,6 +68,7 @@ ActiveRecord::Schema.define(version: 2021_09_20_072800) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
   add_foreign_key "test_passages", "tests"
   add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
