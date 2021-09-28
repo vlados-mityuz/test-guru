969c67e172fa1740571e9534533e0c3408be1a0c
Vladislav <thenoma1@gmail.com>
2021-09-28 19:04:20 +0300

    Replace password with password_digest and add email column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_215033) do
+ActiveRecord::Schema.define(version: 2021_09_28_144551) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -60,10 +60,10 @@ ActiveRecord::Schema.define(version: 2021_09_23_215033) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
-    t.string "password", null: false
-    t.string "role", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "email"
   end
 
   add_foreign_key "answers", "questions"
