a5760df68cde31f4f510c5b055eadf393f162e4b
Vladislav <thenoma1@gmail.com>
2021-09-30 18:30:12 +0300

    Added helper for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def github_url(author, repo, project_name)
     link_to(project_name, "http://github.com/#{author}/#{repo}")
   end
+
+  def flash_messages
+    flash.map do |key, msg|
+      msg
+    end.join
+  end
 end
