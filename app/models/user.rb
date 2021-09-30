aa5e569fdc2fb349cf80d3956084c0cd835218bd
Vladislav <thenoma1@gmail.com>
2021-09-30 18:32:41 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+
   has_many :created_tests, class_name: 'Test', foreign_key: :creator_id
   has_many :test_passages
   has_many :tests, through: :test_passages
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\w+@\w+[[.]\w+]*/i }
+
+  has_secure_password
 
   def show_passed_tests(level)
     tests.where(level: level)
