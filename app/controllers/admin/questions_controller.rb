ee7e1048603bb58f97166f1b4b62ef914ff6c016
Vladislav <thenoma1@gmail.com>
2021-10-06 20:51:58 +0300

    Add answers and questions controllers to admin

---
+++ app/controllers/admin/questions_controller.rb
@@ -0,0 +1,58 @@
+class Admin::QuestionsController < Admin::BaseController
+  before_action :find_question, only: [:show, :destroy, :edit, :update]
+  before_action :find_test, only: [:index, :new, :create]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @question = @test.questions.new(question_params)
+    if @question.save
+      redirect_to [:admin, @question]
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to [:admin, @question]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.delete
+    render plain: "Вопрос удалён"
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: "Запись c id: #{params[:id]} не найдена."
+  end
+
+  def question_params
+    params.require(:question).permit(:body, :test_id)
+  end
+end
