77a7d0925e6e3632f30b097cce99550e53832b5e
Vladislav <thenoma1@gmail.com>
2021-10-17 23:24:33 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
 
+  before_action :set_locale
+
+  def default_url_options
+    { lang: (I18n.locale if I18n.locale != I18n.default_locale) }
+  end
+
   private
   
   def after_sign_in_path_for(resource)
     resource.admin? ? admin_tests_path : super
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
 end
