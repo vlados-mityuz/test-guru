58e96b486b9f66f42bb8d0ca7fb86e580dd36241
Vladislav <thenoma1@gmail.com>
2021-10-26 21:28:01 +0300

    Update git_hub_client.rb

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -3,20 +3,10 @@ class GitHubClient
   ACCESS_TOKEN = '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'
 
   def initialize
-    @http_client = setup_http_client
+    @client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
   end
 
   def create_gist(params)
-    @http_client.post('gists') do |request|
-      request.headers['Authorization'] = "token #{ACCESS_TOKEN}"
-      request.headers['Content-Type'] = 'application/json'
-      request.body = params.to_json
-    end
-  end
-
-  private
-
-  def setup_http_client
-    Faraday.new(url: ROOT_ENDPOINT)
+    @client.create_gist(params)
   end
 end
