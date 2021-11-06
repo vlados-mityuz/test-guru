0712e2bb1013c5db4f47fe6cc514d9d9d00e9146
Vladislav <thenoma1@gmail.com>
2021-11-06 18:58:10 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,17 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test_guru_production"
-
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: { 'test-guru-vlados-mityuz.herokuapp.com' }
+  config.action_mailer.delivery_method = :setuo
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
