91dd17c450e8562f32efa6f1efa9b3a151f0b933
Vladislav <thenoma1@gmail.com>
2021-11-08 20:40:49 +0300

    Fix syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test_guru_production"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: { 'test-guru-vlados-mityuz.herokuapp.com' } }
+  config.action_mailer.default_url_options = { host: 'test-guru-vlados-mityuz.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
