bc9bcac6489e18e598ea0a168bd29925bc84db4d
Vladislav <thenoma1@gmail.com>
2021-11-15 01:07:35 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test', foreign_key: :creator_id
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
-  has_many :gists
+  has_many :gists, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true, format: { with: /\w+@\w+[[.]\w+]*/i }
 
