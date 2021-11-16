f060fe75d2cd6838ec748d5b21b7a46b50592bbe
Vladislav <thenoma1@gmail.com>
2021-11-16 23:56:21 +0300

    Fix tests deletion error

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :gists, dependent: :destroy
+  has_many :current_questions, class_name: 'TestPassage', foreign_key: :current_question_id, dependent: :destroy
   belongs_to :test
 
   validates :body, presence: true
