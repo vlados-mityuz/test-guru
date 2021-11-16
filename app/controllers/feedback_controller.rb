bc364fa51e0dab45b3276b386027afae6ec79547
Vladislav <thenoma1@gmail.com>
2021-11-16 23:49:45 +0300

    Fix feedback sending

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -3,13 +3,7 @@ class FeedbackController < ApplicationController
   def index; end
 
   def create
-    FeedbackMailer.send_feedback_message(feedback_params) unless feedback_params[:message].empty?
+    FeedbackMailer.send_feedback_message(params) unless params[:message].nil?
     redirect_to root_path, notice: t('.notice')
-  end
-
-  private
-
-  def feedback_params
-    params.permit(:message)
   end
 end
