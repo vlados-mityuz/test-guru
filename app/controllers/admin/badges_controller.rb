936d8ab35ca9717312f92ab0207cda18cac7aa38
Vladislav <thenoma1@gmail.com>
2021-11-29 21:46:51 +0300

    Add badges models and controller

---
+++ app/controllers/admin/badges_controller.rb
@@ -0,0 +1,49 @@
+class Admin::BadgesController < ApplicationController
+  before_action :find_badges, only: %i[show edit update destroy]
+
+  def index
+    @badges = Badge.all
+  end
+
+  def show
+  end
+
+  def new
+    @badge = Badge.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @badge.update(badge_params)
+      redirect_to [:admin, @badge]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path
+  end
+
+  def create
+    @badge = Badge.new(badge_params)
+    if @badge.save
+      redirect_to [:admin, @badge]
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_badges
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.require(:badge).permit(:title, :image_url)
+  end
+end
--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,8 +1,28 @@
 class Badge < ApplicationRecord
-  enum rule_type: %i[by_category by_level first_time_try]
-
-  validates :title
-  
-  has_many :badges, dependent: :destroy
   has_many :users, through: :user_badges
+
+  CATEGORIES = ["All SQL tests completed", "Test passed from the first attempt", "Level 1 compleated" ]
+
+  def rule
+    case title
+    when CATEGORIES[0]
+      "all_sql_tests_completed"
+    when CATEGORIES[1]
+      "passed_from_first_attempt"
+    when CATEGORIES[2]
+      "level_completed"
+    end
+  end
+
+  def all_sql_tests_completed_award?(badge_value)
+    badge_value[:all_sql_tests_completed]
+  end
+
+  def passed_from_first_attempt_award?(badge_value)
+    badge_value[:passed_from_first_attempt]
+  end
+
+  def level_completed_award?(badge_value)
+    badge_value[:level_completed]
+  end
 end
