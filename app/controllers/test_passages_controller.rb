35d6b128040d1e1765e6a0787162041540e19b80
Vladislav <thenoma1@gmail.com>
2021-11-29 21:47:02 +0300

    Update test_passages_controller.rb

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -28,6 +28,7 @@ class TestPassagesController < ApplicationController
     @test_passage.accept!(params[:answer_ids])
 
     if @test_passage.completed?
+      BadgesForUsersServices.new(@test_passage.id).call if @test_passage.success?
       TestsMailer.completed_test(@test_passage).deliver_now
       redirect_to result_test_passage_path(@test_passage)
     else
